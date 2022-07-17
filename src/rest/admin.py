34cb5d5e263737e80502c9e2fe969035cb0d3e1a
Aleksandr Zuev <zuev08@gmail.com>
2022-07-17 14:31:05 +0200

    FWC-49 Add __str__ implementations, related picture select

--- src/rest/admin.py
+++ src/rest/admin.py
@@ -1,12 +1,21 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django import forms
 
 from rest import models
+
+class ProductForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['main_picture'].queryset = models.Picture.objects.filter(product_id=self.instance.id)
+
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    form = ProductForm
 
 admin.site.register(models.User, UserAdmin)
 admin.site.register(models.Category)
 admin.site.register(models.Property)
-admin.site.register(models.Product)
 admin.site.register(models.Picture)
 admin.site.register(models.ProductProperty)
 admin.site.register(models.Order)
--- src/rest/models.py
+++ src/rest/models.py
@@ -11,11 +11,17 @@ class Category(models.Model):
     class Meta:
         verbose_name_plural = "categories"
 
+    def __str__(self):
+        return self.name
+
 class Property(models.Model):
     name = models.CharField(max_length=80)
 
     class Meta:
         verbose_name_plural = "properties"
+
+    def __str__(self):
+        return self.name
 
 class Product(models.Model):
     name = models.CharField(max_length=128)
@@ -23,6 +29,9 @@ class Product(models.Model):
     price = models.PositiveBigIntegerField(help_text="Милликопейки (100000 = 1 руб.)")
     main_picture = models.ForeignKey('Picture', on_delete=models.SET_NULL, null=True, blank=True, related_name='+')
     properties = models.ManyToManyField(Property, through='ProductProperty', related_name='products')
+
+    def __str__(self):
+        return self.name
 
 class Picture(models.Model):
     image = models.ImageField()
@@ -40,7 +49,21 @@ class ProductProperty(models.Model):
         ]
         verbose_name_plural = "product properties"
 
+    def get_value(self):
+        if self.num_value is not None:
+            result = str(self.num_value)
+            if self.string_value is not None and len(self.string_value) > 0:
+                result += f" {self.string_value}"
+            return result
+        else:
+            return self.string_value
+
+    def __str__(self):
+        return f"{self.property}: {self.get_value()} ({self.product})"
+
 class Order(models.Model):
+    of_user_label = _("of user")
+
     class Status(models.TextChoices):
         OPEN = 'OPEN', _("Open")
         CLOSED = 'CLOSED', _("Closed")
@@ -49,7 +72,13 @@ class Order(models.Model):
     products = models.ManyToManyField(Product, through='OrderItem', related_name='+')
     status = models.CharField(max_length=16, choices=Status.choices, default=Status.OPEN)
 
+    def __str__(self):
+        return f"{self.__class__.__name__} {self.of_user_label} {self.user} ({self.pk})"
+
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='order_items')
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='order_items')
     quantity = models.PositiveSmallIntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.quantity} x {self.product} ({self.order})"
