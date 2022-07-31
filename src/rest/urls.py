2c8f1c1917398c289ee42c98d58a3b7613bac5d5
Aleksandr Zuev <zuev08@gmail.com>
2022-07-31 13:20:19 +0200

    FWC-59 Remove old unused endpoints

--- src/rest/urls.py
+++ src/rest/urls.py
@@ -10,8 +10,5 @@ urlpatterns = [
     path('', views.TestView.as_view(), name='test'),
     path('graphql', csrf_exempt(GraphQLView.as_view(graphiql=True))),
     path('csrftoken/', views.CsrfTokenView.as_view()),
-    path('login/', views.LoginView.as_view()),
-    path('logout/', views.LogoutView.as_view()),
-    path('account/', views.AccountView.as_view()),
     path('accounts/', include('rest_registration.api.urls')),
 ]
--- src/rest/views.py
+++ src/rest/views.py
@@ -19,32 +19,3 @@ class TestView(APIView):
 class CsrfTokenView(APIView):
     def get(self, request):
         return Response({'csrftoken': 'set in the cookie'})
-
-@method_decorator(ensure_csrf_cookie, name='dispatch')
-class LoginView(GenericAPIView):
-    serializer_class = serializers.LoginSerializer
-
-    def post(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        data = serializer.data
-
-        username = data['login']
-        password = data['password']
-        user = authenticate(username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return Response({'result': 'success'})
-
-        raise ParseError("invalid credentials")
-
-class LogoutView(APIView):
-    def post(self, request):
-        logout(request)
-        return Response({'result': 'success'})
-
-class AccountView(RetrieveAPIView):
-    serializer_class = serializers.AccountSerializer
-
-    def get_object(self):
-        return self.request.user
