9cf98c9d781996b5f68cc0f3bb0fc31ef783ed39
MaksimDyumin <68166387+MaksimDyumin@users.noreply.github.com>
2022-08-28 14:54:23 +0300

    local changes

--- src/fishing/settings/dev.py
+++ src/fishing/settings/dev.py
@@ -1,1 +1,18 @@
 from fishing.settings.base import *
+
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "localhost",
+]
+CSRF_TRUSTED_ORIGINS = [
+    "http://127.0.0.1:3000",
+    "http://localhost:3000",
+    "http://127.0.0.1:8000",
+    "http://localhost:8000",
+]
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+    "http://127.0.0.1:3000",
+    "http://127.0.0.1:8000",
+    "http://localhost:8000",
+]
